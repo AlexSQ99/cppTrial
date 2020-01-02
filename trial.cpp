cc2022e31f5705d20494b8e38591b9ac51cba582
AlexSQ99 <59295023+AlexSQ99@users.noreply.github.com>
2020-01-02 17:43:06 -0600

    Added trial.cpp

---
+++ trial.cpp
@@ -0,0 +1,21 @@
+//
+//  trial.cpp
+//  trial1
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Let's Count! (And hope GitHub works) " << endl;
+    
+    for (int i=0; i <= 200; i++)
+    {
+        cout << "Here is number: " << i << endl;
+    }
+    
+    return 0;
+}
